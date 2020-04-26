36e3550a52b0f3d73378ba94be52f58467249731
Legend <maesabroso@gmail.com>
2020-04-25 21:59:58 -0500

    filter funtion is working

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Jumbotron from "./components/Jumbo/Jumbotron.js";
 import Container from "./components/Container/index.js";
 import Row from "react-bootstrap/Row";
-import employees from "./components/employee/employee.json"
-import THead from "./components/Table/THead.js";
 import Col from "react-bootstrap/Col";
-import TBody from "./components/Table/TBody.js";
-import Table from "react-bootstrap/Table"
-import Button from "react-bootstrap/Button"
 
-class App extends Component {
-  
-  state = {
-    employees: employees,
-    displayEmployees: employees
-  };
-  
-  filterByName = event => {
-    this.setState({ ...this.state, displayEmployees: this.state.employees.filter(entry => entry.name.indexOf('J') !== -1) })
-  }
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Jumbotron />
-            <Button onClick={this.filterByName}>Filter</Button>
-            <Table responsive hover>
-              <THead />
-              {this.state.displayEmployees.map(employees => (
-                <TBody
-                  image={employees.image}
-                  name={employees.name}
-                  phone={employees.phone}
-                  email={employees.email}
-                  DOB={employees.DOB}
-                />
-              ))}
-            </Table>
-            <hr />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+function App () {
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Jumbotron />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
--- src/components/Jumbo/Jumbotron.js
+++ src/components/Jumbo/Jumbotron.js
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import employees from "../employee/employee.json";
 import {Row, Col} from "react-bootstrap";
 import Title from "../Title/index.js";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import THead from "../Table/THead";
+import TBody from "../Table/TBody";
+import Table from "react-bootstrap/Table"
 import "./style.css";
 
-const Jumbotron = () => {
-  return (
-    <Row>
-      <Col className="text-white text-center py-5 px-4 my-5" style={{ backgroundImage: `url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg)` }}>
-        <Col className="py-5">
-          <Title>Employee Directory</Title>
-          <p className="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-          optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
+class Jumbotron extends Component {
+
+  state = {
+    employees: employees,
+    displayEmployees: employees,
+    search: ""
+  };
+
+  filterByName = event => {
+    this.setState({ ...this.state, search: event.target.value ,displayEmployees: this.state.employees.filter(entry => entry.name.toLowerCase().indexOf(this.state.search) !== -1) })
+  }
+
+  resetFilter = event =>{
+    window.location.reload()
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col className="text-white text-center py-5 px-4 my-5" style={{ backgroundImage: `url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg)` }}>
+          <Col className="py-5">
+            <Title>Employee Directory</Title>
+            <p className="mx-5 mb-5">Use the search box to narrow your results
             </p>
-          <Col md={{ span: 6, offset: 3 }}>
-            <InputGroup className="mb-3">
-              <FormControl id="searchBar" placeholder=""/>
-              <InputGroup.Append>
-                <Button variant="outline-secondary">Search</Button>
-              </InputGroup.Append>
-            </InputGroup>
+            <Col md={{ span: 4, offset: 4 }}>
+              <InputGroup className="mb-3">
+                <FormControl className="mr-1" placeholder="type to filter by Name..." type="text" value={this.state.search} onChange={this.filterByName} />
+                <InputGroup.Append>
+                  <Button variant="outline-dark" onClick={this.resetFilter}>Reset</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Col>
           </Col>
         </Col>
-      </Col>
-    </Row>
-  )
+
+        <Table responsive hover>
+          <THead />
+          {this.state.displayEmployees.map(employees => (
+            <TBody
+              image={employees.image}
+              name={employees.name}
+              phone={employees.phone}
+              email={employees.email}
+              DOB={employees.DOB}
+            />
+          ))}
+        </Table>
+      </Row>
+    )
+  }
 }
 
 export default Jumbotron;
